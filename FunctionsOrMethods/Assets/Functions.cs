ff65152ca4cf27d24d68a4b2c681a31be5bc82e6
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-14 14:46:50 +0300

    Functions or Methods
    https://www.youtube.com/watch?v=MCrEhTHklsQ&list=PLadYLGMfR6Lpvh3xX3a7LXbqJqY_WjIk7&index=1

---
+++ FunctionsOrMethods/Assets/Functions.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    public int a = 5;
+    public int b = 5;
+    public int total;
+
+    public GameObject cube;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            total = Multiply(96,b);
+            Debug.Log("Total:" + total);
+
+            cube.GetComponent<Renderer>().material.color = ChangeColor(Color.red,cube,"Unity3D");
+        }
+	}
+
+    public int Multiply(int a,int b)
+    {
+        return a * b;
+    }
+
+    void TellMePosition(GameObject go,string message)
+    {
+        Debug.Log("Position:" + go.transform.position + " " + message);
+    }
+
+    Color ChangeColor(Color newColor,GameObject go,string name)
+    {
+        go.name = name;
+        return newColor;
+    }
+}
