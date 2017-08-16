d9a68fde37f497920644f646e1dee2220dae9736
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-17 01:45:08 +0300

    Update Player.cs

--- Constructors/Assets/Player.cs
+++ Constructors/Assets/Player.cs
@@ -1,21 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Item[] items=new Item[5];
+    public Item[] items = new Item[5];
+    public Job[] jobs = new Job[1];
 
-    public Job[] jobs=new Job[1]; 
-
-	void Start ()
+    void Start()
     {
-        items[0] = new Item ( 0, "Long sword", "Very sharp" );
+        items[0] = new Item(0, "Long sword", "Very sharp");
         items[1] = new Item(1, "Bronze helmet");
 
         jobs[0] = new Job("Software Engineer", "I program", 10000);
     }
-	
-	void Update ()
+
+    void Update()
     {
-		
-	}
+
+    }
 }
