f614993e7c6c2437fac2c5d9759c1e6a89ba5c2f
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-19 19:08:28 +0300

    Properties
    https://www.youtube.com/watch?v=k-K2JWoEaS8&list=PLadYLGMfR6Lpvh3xX3a7LXbqJqY_WjIk7&index=5

---
+++ Properties/Assets/MyPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    private Properties myProperties;
+
+    void Start()
+    {
+        myProperties = new Properties();
+        Debug.Log(myProperties.MyAge);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Properties/Assets/Properties.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Properties 
+{
+    private int _myAge = 26;
+
+    public int MyAge
+    {
+        get
+        {
+            return _myAge;
+        }
+        set
+        {
+            _myAge = value;
+        }
+    }
+}
