5474cdcdeba8ecefbf36e86e9ecd9539468b6320
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-19 17:24:33 +0300

    StaticTypes
    https://www.youtube.com/watch?v=RSx3jt9epUQ&index=4&list=PLadYLGMfR6Lpvh3xX3a7LXbqJqY_WjIk7

---
+++ StaticTypes/Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StaticExample.myInt = 0;
+        transform.position = StaticExample.SetPositionZero();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ StaticTypes/Assets/StaticExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticExample : MonoBehaviour
+{
+    public static int myInt;
+
+    public static Vector3 SetPositionZero()
+    {
+        return Vector3.zero;
+    }
+
+       void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
